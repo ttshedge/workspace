46051d4998a1789275114b241c1a5a03f4999048
Tushar <shedgetushar.22@gmail.com>
2022-06-09 22:37:26 +0530

    base

---
+++ First/src/test/java/com/tts/first/FirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.first;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
